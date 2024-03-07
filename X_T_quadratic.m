13bc7201e1a3f8d407c17e3e146cb7a0adcfc814
ROGEEER8 <160430904+ROGEEER8@users.noreply.github.com>
2024-03-08 00:01:38 +0100

    Update X_T_quadratic.m

--- X_T_quadratic.m
+++ X_T_quadratic.m
@@ -1,41 +1,32 @@
 function [X,T] = X_T_quadratic(n_elem)
 
-n_nodes = 8 + (n_elem - 1) * 5;
+    n_nodes = 8 + (n_elem - 1) * 5;
+    
+    X = zeros(n_nodes,2);
+    T = zeros(n_elem,8+3); 
+    
+    %Nodal Coordinates Matrix
+    
+    % 1st column
+    X(1:2*n_elem+1,1) = 0:0.5:n_elem;
+    X(2*n_elem+2:(3*n_elem+2),1) = 0:n_elem;
+    X((3*n_elem+3):n_nodes,1) = 0:0.5:n_elem;
+    
+    %X 2nd column
+    X(1:2*n_elem+1,2) = 0;
+    X(2*n_elem+2:(3*n_elem+2),2) = 0.5;
+    X((3*n_elem+3):n_nodes,2) = 1;
+    
+    %Connectivity Matrix
+    
+    T(1:n_elem,1:3) = [1:2:2*n_elem - 1; 2:2:(2*n_elem);3:2:(2*n_elem + 1)]';
+    T(1:n_elem,4) = (T(n_elem,3)+2):(T(n_elem,3)+1+n_elem);
+    T(1:n_elem,5) = (T(n_elem,4)+3):2:(n_nodes);
+    T(1:n_elem,6) = (T(n_elem,4)+2):2:(n_nodes-1);
+    T(1:n_elem,7) = (T(n_elem,4)+1):2:(n_nodes-2);
+    T(1:n_elem,8) = (T(n_elem,3)+1):(T(n_elem,3)+n_elem);
 
-X = zeros(n_nodes,2);
-T = zeros(n_elem,8+3); 
-
-%%
-
-%Nodal Coordinates Matrix
-
-%1st column
-X(1:2*n_elem+2,1) = (repmat(0:n_elem,[1,2]))';
-X(2*n_elem+2 + 1:2*n_elem+2 + 1 + n_elem - 1,1) = 0.5:n_elem-0.5;
-X(2*n_elem+2 + 1 + n_elem:2*n_elem+2 + 1 + n_elem * 2,1) = 0:n_elem;
-X(2*n_elem+2 + 2 + n_elem * 2:2*n_elem+2 + 2 + n_elem * 3 - 1,1) = 0.5:n_elem-0.5;
-
-%2nd column
-X(1:n_elem + 1,2) = zeros(n_elem + 1, 1);
-X(n_elem + 2 : (n_elem + 1) * 2,2) = ones(n_elem + 1, 1);
-X((n_elem + 1) * 2 + 1:n_elem * 4 + n_elem*-1 + 2,2) = zeros(n_elem,1);
-X(n_elem * 4 + n_elem*-1 + 3:n_elem * 5 + n_elem*-1 + 3,2) = zeros(n_elem + 1,1) + 0.5;
-X(n_elem * 5 + n_elem*-1 + 4:8 + 5* (n_elem - 1),2) = ones(n_elem,1);
-
-%%
-
-%Connectivity matrix
-
-T(:,1) = (1:n_elem)';
-T(:,2) = ((n_elem + 1) * 2 + 1:n_elem * 4 + n_elem*-1 + 2)';
-T(:,3) = (2:n_elem+1)';
-T(:,4) = ((n_elem + 1) * 2 + 2+n_elem:n_elem * 4 + n_elem*-1 + 2 +n_elem + 1)';
-T(:,5) = (3*n_elem + 1 + (-2*(n_elem - 1)):4*n_elem + (-2*(n_elem-1)));
-T(:,6) = ((n_elem + 1) * 2 + 2+n_elem + n_elem:n_elem * 4 + n_elem*-1 + 2 +n_elem + 1 + n_elem)';
-T(:,7) = (2+n_elem:2*n_elem+1)';
-T(:,8) = ((n_elem + 1) * 2 + 1 + n_elem:n_elem * 4 + n_elem*-1 + 2 + n_elem)';
-
-
+end
 
 
 
