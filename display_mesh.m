f92b0156558103ffc93ce1328fb3b877083e9aa0
ROGEEER8 <160430904+ROGEEER8@users.noreply.github.com>
2024-03-08 00:03:24 +0100

    Update display_mesh.m

--- display_mesh.m
+++ display_mesh.m
@@ -1,7 +1,27 @@
 clear all; close all; clc
 
-nOfElements = 200;
 
-degree = 1;
+%% 1D mesh - degree 1 quadrilaterals
+nOfElements = 10;
+[X,T] = X_T(nOfElements); % Standard 1D quadrilaterals degree 1 mesh
+figure(1),clf,kk = plotMesh_2(X,T(:,1:nnz(T(1,:))),'plotNodes');
+
+%% 1D mesh - degree 1 quadrilaterals made of degree 1 triangles
+nOfElements = 20;
 [X,T] = X_T_squared_triangles_squares(nOfElements);
-figure(1),clf,kk = plotMesh(X,T(:,1:nnz(T(1,:))),'plotNodes'); 
+figure(3),clf,kk = plotMesh(X,T(:,1:nnz(T(1,:))),'plotNodes');
+
+
+%% 1D mesh - degree 2 quadrilaterals
+nOfElements = 10;
+[X,T] = X_T_quadratic(nOfElements);
+figure(1),clf,kk = plotMesh_3(X,T(:,1:nnz(T(1,:))),'plotNodes');
+
+
+%% 2D mesh - degree 1 quadrilaterals
+
+L = 1;
+n = 10;
+[X,T] = X_T_Square_Surface(L,n);
+figure(4); kk = plotMesh(X,T(:,1:nnz(T(1,:))),'plotNodes');
+
