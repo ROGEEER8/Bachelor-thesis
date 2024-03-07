01ceb917bce4edbdda3f17c320b3185ac1ebee62
ROGEEER8 <160430904+ROGEEER8@users.noreply.github.com>
2024-03-08 00:01:21 +0100

    Update X_T_Square_Surface.m

--- X_T_Square_Surface.m
+++ X_T_Square_Surface.m
@@ -1,37 +1,43 @@
-function [X,T] = X_T_Square_Surface(side)
+function [X,T] = X_T_Square_Surface(L,n)
 
-
-nOfElements = side^2;
-n_nodes = (side + 1) ^ 2;
-
-X = zeros(n_nodes,2);
-
-X(:,1) = repmat(0:nOfElements/side,1,side + 1)';
-X(:,2) = repelem(0:side,1,side + 1);
-
-a = zeros(side,4);
-b = side + 1;
-c = side - 1;
-
-a(1:side,1) = (1:side)';
-a(1:side,2) = a(1:side,1) + 1;
-a(1:side,3) = a(1:side,2) + b;
-a(1:side,4) = a(1:side,3) - 1;
-
-T = repmat(a,side,1);
-
-j_prev = 1;
-j = side;
-
-for i = 0:c
-    if i == 0
-       T(j_prev:j,:) = T(j_prev:j,:) + 4*i;
-    elseif i ~= 0
-    T(j_prev:j,:) = T(j_prev:j,:) + 4*i + 2*i; 
+    h = L/n; %Length of each element given n and L
+    nOfElements = n^2;
+    n_nodes = (n + 1) ^ 2;
+    
+    
+    %% X, nodal coordinate matrix
+    
+    X = zeros(n_nodes,2);
+    
+    X(:,1) = repmat(0:h:L,1,n+1)';
+    X(:,2) = repelem(0:h:L,1,n+1);
+    
+    %% T, connectivity matrix
+    a = zeros(n,4);
+    b = n + 1;
+    c = n - 1;
+    
+    a(1:n,1) = (1:n)';
+    a(1:n,2) = a(1:n,1) + 1;
+    a(1:n,3) = a(1:n,2) + b;
+    a(1:n,4) = a(1:n,3) - 1;
+    
+    T = repmat(a,n,1);
+    
+    j_prev = 1;
+    j = n;
+    
+    for i = 0:c
+        if i == 0
+           T(j_prev:j,:) = T(j_prev:j,:);
+        elseif i ~= 0
+        T(j_prev:j,:) = T(j_prev:j,:) + (n+1)*i; 
+        end
+    j_prev = j + 1;
+    j = j_prev + n - 1;
     end
-j_prev = j + 1;
-j = j_prev + side - 1;
-end
+    
+    T = [T zeros(nOfElements,3)];
 
 end
 
